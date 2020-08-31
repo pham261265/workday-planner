fcbde1e8281cfe53eb237ecb2aeb736ec62bb70e
niku senpai <pbpham97@gmail.com>
2020-08-30 23:27:58 -0500

    added more rows

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var date = moment().format("MMM Do YYYY");
 
 $("#currentDay").append(date);
 
-var timeOfday = ["7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19"]
+var timeOfday = ["7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
 updatetime();
 
 function updatetime() {
@@ -59,3 +59,5 @@ $("#5pm").children("input").val(localStorage.getItem("5pm"));
 $("#6pm").children("input").val(localStorage.getItem("6pm"));
 
 $("#7pm").children("input").val(localStorage.getItem("7pm"));
+
+$("#8pm").children("input").val(localStorage.getItem("8pm"));
